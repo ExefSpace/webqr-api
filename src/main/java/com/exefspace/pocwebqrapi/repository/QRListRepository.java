7e3586bf81783b3977474fab026124b6d7c71991
Nik Quito Espinoza <nik.quito@unmsm.edu.pe>
2021-02-24 23:03:05 -0500

    actualizada api mix query interfaces

--- src/main/java/com/exefspace/pocwebqrapi/repository/QRListRepository.java
+++ src/main/java/com/exefspace/pocwebqrapi/repository/QRListRepository.java
@@ -1,7 +1,14 @@
 package com.exefspace.pocwebqrapi.repository;
 
+import com.exefspace.pocwebqrapi.model.IQRListCanales;
 import com.exefspace.pocwebqrapi.model.QRList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface QRListRepository  extends JpaRepository<QRList,Integer> {
+
+    @Query(value="SELECT  q.idQR, q.url, q.idCanal, q.creationUser, q.creationDate, q.updateUser, q.updateDate, c.idTipoCanal, c.CodigoCanal, c.DescripcionCanal, c.DescripcionUbicacion,t.DescripcionTipoCanal FROM QRList q inner join Canales c on q.IdCanal=c.IdCanal inner join TipoCanales t on c.IdTipoCanal=t.IdTipoCanal", nativeQuery = true)
+    List<IQRListCanales> obtenerQrXCanalTodos();
 }
