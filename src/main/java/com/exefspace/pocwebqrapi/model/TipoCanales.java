690d1c09d33907854e3d0e02da9fa1e84e66f5c5
Nik Quito Espinoza <nik.quito@unmsm.edu.pe>
2021-03-02 16:49:38 -0500

    Actualizado tipo canales model

--- src/main/java/com/exefspace/pocwebqrapi/model/TipoCanales.java
+++ src/main/java/com/exefspace/pocwebqrapi/model/TipoCanales.java
@@ -3,6 +3,7 @@ package com.exefspace.pocwebqrapi.model;
 import org.hibernate.annotations.Nationalized;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "TipoCanales")
@@ -10,11 +11,21 @@ public class TipoCanales {
 
     private Integer IdTipoCanal;
     private String DescripcionTipoCanal;
-
+    private String creationUser;
+    private Timestamp creationDate;
+    private String updateUser;
+    private Timestamp updateDate;
     public TipoCanales(){}
-    public TipoCanales(String descripcionTipoCanal) {
+
+    public TipoCanales( String descripcionTipoCanal, String creationUser, Timestamp creationDate, String updateUser, Timestamp updateDate) {
         DescripcionTipoCanal = descripcionTipoCanal;
+        this.creationUser = creationUser;
+        this.creationDate = creationDate;
+        this.updateUser = updateUser;
+        this.updateDate = updateDate;
     }
+
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -36,5 +47,43 @@ public class TipoCanales {
 
     public void setDescripcionTipoCanal(String descripcionTipoCanal) {
         DescripcionTipoCanal = descripcionTipoCanal;
+    }
+
+    @Column(name = "creationUser", nullable = false)
+    @Nationalized
+    public String getCreationUser() {
+        return creationUser;
+    }
+
+    public void setCreationUser(String creationUser) {
+        this.creationUser = creationUser;
+    }
+    @Column(name = "creationDate", nullable = false)
+    @Nationalized
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @Column(name = "updateUser", nullable = false)
+    @Nationalized
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+    @Column(name = "updateDate", nullable = false)
+    @Nationalized
+    public Timestamp getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Timestamp updateDate) {
+        this.updateDate = updateDate;
     }
 }
